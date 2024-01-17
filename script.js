f19e9875c226a721a70a0e7e6916456bf81dc91d
DyrFox <102378846+Gabin-SUBRI@users.noreply.github.com>
2024-01-17 18:08:19 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -17,3 +17,52 @@ document.addEventListener("DOMContentLoaded", function () {
   // Appelez la fonction pour mettre à jour l'âge au chargement de la page
   augmenterAgeChaque15Juillet();
 });
+
+// le boutton des textes
+function showFullText() {
+  var shortText = document.getElementById("shortText");
+  var fullText = document.getElementById("fullText");
+
+  shortText.style.display = "none";
+  fullText.style.display = "block";
+}
+
+function showFullText2() {
+  var shortText = document.getElementById("shortText2");
+  var fullText = document.getElementById("fullText2");
+
+  shortText.style.display = "none";
+  fullText.style.display = "block";
+}
+
+function showFullText3() {
+  var shortText = document.getElementById("shortText3");
+  var fullText = document.getElementById("fullText3");
+
+  shortText.style.display = "none";
+  fullText.style.display = "block";
+}
+
+function showFullText4() {
+  var shortText = document.getElementById("shortText4");
+  var fullText = document.getElementById("fullText4");
+
+  shortText.style.display = "none";
+  fullText.style.display = "block";
+}
+
+function showFullText5() {
+  var shortText = document.getElementById("shortText5");
+  var fullText = document.getElementById("fullText5");
+
+  shortText.style.display = "none";
+  fullText.style.display = "block";
+}
+
+function showFullText6() {
+  var shortText = document.getElementById("shortText6");
+  var fullText = document.getElementById("fullText6");
+
+  shortText.style.display = "none";
+  fullText.style.display = "block";
+}
