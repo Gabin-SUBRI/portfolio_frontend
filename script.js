b2322c9ee6d5260bfcfd08dc13ad559d73675fc8
Gabin Subri <gabin.subri@edu.igensia.com>
2025-02-07 14:05:46 +0100

    Modification du texte présentation pour que les lignes face 30 caractères peut importe la taille de l'écran

--- script.js
+++ script.js
@@ -13,31 +13,31 @@ document.addEventListener("DOMContentLoaded", function () {
   const fullText = `Etudiant en développement web passionné par la création de solutions web performantes et innovantes. Spécialisé en HTML, CSS et Javascript, avec 1 ans d’expérience en Analyste et développement d'applications. Toujours prêt pour de nouveaux défis technologiques.`;
   let charIndex = 0;
 
-  function getMaxLineLength() {
-    return window.matchMedia("(max-width: 768px)").matches ? 30 : 50;
-  }
-
   function typeWriter() {
     if (charIndex < fullText.length) {
       textElement.textContent += fullText.charAt(charIndex);
       charIndex++;
 
-      const maxLineLength = getMaxLineLength();
+      // Vérifier la longueur de la ligne actuelle sans couper les mots
+      const lines = textElement.textContent.split("\n");
+      const currentLine = lines[lines.length - 1];
 
-      // Ajouter un saut de ligne si on atteint la longueur maximale de la ligne sans couper les mots
-      if (charIndex % maxLineLength === 0) {
-        const lastSpaceIndex = textElement.textContent.lastIndexOf(" ");
+      if (currentLine.length >= 30) {
+        const lastSpaceIndex = currentLine.lastIndexOf(" ");
         if (lastSpaceIndex !== -1) {
           textElement.textContent =
-            textElement.textContent.slice(0, lastSpaceIndex) +
+            textElement.textContent.slice(
+              0,
+              textElement.textContent.lastIndexOf(" ")
+            ) +
             "\n" +
-            textElement.textContent.slice(lastSpaceIndex + 1);
-        } else {
-          textElement.textContent += "\n";
+            textElement.textContent.slice(
+              textElement.textContent.lastIndexOf(" ") + 1
+            );
         }
       }
 
-      setTimeout(typeWriter, 30);
+      setTimeout(typeWriter, 20);
     } else {
       textElement.style.border = "none"; // Supprime le curseur clignotant après animation
     }
